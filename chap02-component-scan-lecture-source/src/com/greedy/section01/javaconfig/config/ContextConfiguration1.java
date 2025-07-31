28254e593db92508844d24288b993c676e1a5ad8
sooyoung <sim980416@gmail.com>
2025-07-31 22:47:00 +0900

    component-scan-lecture-source

--- chap02-component-scan-lecture-source/src/com/greedy/section01/javaconfig/config/ContextConfiguration1.java
+++ chap02-component-scan-lecture-source/src/com/greedy/section01/javaconfig/config/ContextConfiguration1.java
@@ -3,8 +3,8 @@ package com.greedy.section01.javaconfig.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//@ComponentScan(basePackages = "com.greedy.section01.javaconfig")
+@Configuration
+@ComponentScan(basePackages = "com.greedy.section01.javaconfig")
 public class ContextConfiguration1 {
 	
 }
