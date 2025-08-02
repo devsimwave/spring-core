fc24d23f05e4eaa0eaf6020c4e8ce19a76603070
sooyoung <sim980416@gmail.com>
2025-08-02 18:39:08 +0900

    chap06-bean-scope-lecture-source

---
+++ chap06-bean-scope-lecture-source/src/com/greedy/section02/prototype/xmlconfig/Beverage.java
@@ -0,0 +1,27 @@
+package com.greedy.section02.prototype.xmlconfig;
+
+public class Beverage extends Product {
+	
+	private int capacity;
+	
+	public Beverage() {}
+	
+	public Beverage(String name, int price, int capacity) {
+		super(name, price);
+		this.capacity = capacity;
+	}
+	
+	public int getCapacity() {
+		return this.capacity;
+	}
+	
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " " + this.capacity;
+	}
+	
+}
